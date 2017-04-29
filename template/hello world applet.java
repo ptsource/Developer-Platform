aad7f067be5af4538937ef6cc64659ddd5bbc4d6
PTSource <flirtsml@gmail.com>
2017-04-29 13:56:44 +0100

    Delete hello world applet.java

--- template/hello world applet.java
+++
@@ -1,9 +0,0 @@
-// basic template file for java applet
-
-import java.awt.Graphics;
-
-public class AppletHelloWorld extends java.applet.Applet {
-	public void paint(Graphics g) {
-		g.drawString("hello world", 10, 10);
-	}
-}
